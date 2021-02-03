cb600113871d2310a9210d0fbaf96f4d932fed4c
Zippylicious <zakthompson97@gmail.com>
2021-02-02 20:43:35 -0500

    img folder w/ ignored contents

--- client/src/about.js
+++ client/src/about.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import portrait from './img/portrait.jpg';
 
 class About extends Component {
 
   render() {
     return (
-      <p>ABOUT</p>
+      <div>
+      	<img src={portrait} alt="John Thompson"/>
+      	<p>
+      		Lorem ipsum dolor
+      	</p>
+      </div>
     )
   }
 }
