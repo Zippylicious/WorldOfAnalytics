a7fd2ef0a4a51123af7197bc0a9b8f8e3f4a7b85
Zippylicious <zakthompson97@gmail.com>
2021-02-05 15:40:31 -0500

    Implemented blog post full view mode

--- client/src/blog/blog.js
+++ client/src/blog/blog.js
@@ -7,10 +7,14 @@ class Blog extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			posts: []
+			posts: [],
+			isPreview: 1,
+			post: {}
 		};
 
 		this._truncateBody = this._truncateBody.bind(this);
+		this._switchToPost = this._switchToPost.bind(this);
+		this._switchToPreview = this._switchToPreview.bind(this);
 	}
 
 	_truncateBody(body) {
@@ -22,6 +26,26 @@ class Blog extends Component {
 		return body.slice(0, characterLimit) + "...";
 	}
 
+	_switchToPost(postId) {
+
+		var i;
+		for(i = 0; i < this.state.posts.length; i++) {
+			if(this.state.posts[i]._id === postId) {
+				this.setState({
+					isPreview: 0,
+					post: this.state.posts[i]
+				});
+			}
+		}
+	}
+
+	_switchToPreview() {
+		this.setState({
+			isPreview: 1,
+			post: {}
+		});
+	}
+
 	componentDidMount() {
 		axios.get("http://localhost:9000/blog").then((response) => {
 			this.setState({posts: response.data});
@@ -31,13 +55,24 @@ class Blog extends Component {
 	render() {
 		return (
 			<div>
-				{this.state.posts.map((post) => 
-					<div className="preview" name="preview" key={post._id}>
-						<p class="previewTitle">{post.title}</p>
-						<p>{post.byline}</p>
-						<p>{this._truncateBody(post.body)}</p>
+				{ (this.state.isPreview === 1) ?
+					<div>
+						{this.state.posts.map((post) => 
+							<div className="preview" name="preview" key={post._id} onClick={() => this._switchToPost(post._id)}>
+								<p class="previewTitle">{post.title}</p>
+								<p>{post.byline}</p>
+								<p>{this._truncateBody(post.body)}</p>
+							</div>
+						)}
 					</div>
-				)}
+					:
+					<div>
+						<button onClick={this._switchToPreview}>Back</button>
+						<p class="postTitle">{this.state.post.title}</p>
+						<p>{this.state.post.byline}</p>
+						<p>{this.state.post.body}</p>
+					</div>
+				}
 			</div>
 		)
 	}
